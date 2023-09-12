2d051f6943d7321732cca04b3981d2757945bc9b
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-12 16:33:45 +0930

    chore: updated account slice to RTK

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -19,6 +19,7 @@ function AccountOperations() {
 	function handleDeposit() {
 		if (!depositAmount) return;
 		dispatch(deposit(depositAmount, currency));
+		// dispatch(deposit(depositAmount));
 		setDepositAmount('');
 		setCurrency('USD');
 	}
